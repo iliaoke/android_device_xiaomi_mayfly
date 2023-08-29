78461e8695a071a67e4577e5355a0013093b5778
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 15:57:49 -0500

    mayfly: Add vendor soong imports to extract
    Change-Id: I7b290a9412bd11bb5577ce634a0291ffca40b283

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -5,6 +5,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function vendor_imports() {
+    cat << EOF >> "$1"
+		"device/xiaomi/sm8450-common",
+		"hardware/qcom-caf/sm8450",
+		"hardware/xiaomi",
+		"vendor/qcom/opensource/commonsys-intf/display",
+EOF
+}
+
 set -e
 
 # Required!
