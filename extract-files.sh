c70db65ab10fd3b3d843118c79b8f390ef16dc5d
Arian <arian.kulmer@web.de>
2024-08-29 18:58:19 +0200

    mayfly: Fix sonames of extracted blobs
    Change-Id: Ia4327e5b64e705da6dff6da01d2c9f16dfd609c5

--- extract-files.sh
+++ extract-files.sh
@@ -15,6 +15,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
             ;;
+        vendor/lib64/hw/fingerprint.goodix_fod.default.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF_0_17_2}" --set-soname "fingerprint.goodix_fod.default.so" "${2}"
+            ;;
         vendor/lib64/libcamximageformatutils.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
