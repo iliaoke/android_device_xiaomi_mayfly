5acf221fc6ad897ab771aeb2f71fca3e06c53db5
Arian <arian.kulmer@web.de>
2025-05-13 18:45:01 +0200

    mayfly: Migrate sensor-notifer config to soong_config_set
    Change-Id: I75975e686a3418cddb2b2a2483afbb74d157bc0e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,9 +9,6 @@ DEVICE_PATH := device/xiaomi/mayfly
 # Inherit from sm8450-common
 include device/xiaomi/sm8450-common/BoardConfigCommon.mk
 
-# Light
-TARGET_SENSOR_NOTIFIER_EXT := //device/xiaomi/mayfly:libsensor-notifier-ext-light
-
 # Audio
 AUDIO_FEATURE_ENABLED_ULTRASOUND_PROXIMITY := true
 
--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/resourcemanager_waipio_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/resourcemanager_waipio_mtp.xml \
     $(LOCAL_PATH)/audio/usecaseKvManager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usecaseKvManager.xml
 
+# Light
+$(call soong_config_set,xiaomi_sm8450_sensor_notifier,extension_lib,//device/xiaomi/mayfly:libsensor-notifier-ext-light)
+
 # Overlay
 PRODUCT_PACKAGES += \
     ApertureResMayfly \
